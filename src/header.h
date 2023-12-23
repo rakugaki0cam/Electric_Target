8cff94a96eb8ac9de1c5f1b3623b990bc34a61c9
rakugaki0cam <isober21@gmail.com>
2023-12-23 16:40:39 +0900

    ver.0.12

---
+++ src/header.h
@@ -0,0 +1,33 @@
+/***************************************************************************
+ *  header.h
+ * 
+ * 
+ ***************************************************************************
+*/
+
+#ifndef _HEADER_H    /* Guard against multiple inclusion */
+#define _HEADER_H
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+//#include "ip5306_i2c.h"
+
+
+void ip5306_init(void);
+bool ip5306_read_status(void);
+
+void battery_voltage_disp(bool);
+bool i2c1_bus_check(void);
+bool i2c1_error(void);
+bool i2c1_wait(void);
+
+void deep_sleep(void);
+
+
+#endif // _HEADER_H
+
+/* *****************************************************************************
+ End of File
+ */
